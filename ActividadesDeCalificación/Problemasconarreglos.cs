74120544fb40c8d2c5b83b03cd77b3b126640706
Saul Emmanuel Zuniga Villatoro <saulzuniga606@gmail.com>
2021-08-10 21:15:36 -0600

    Agregar archivos de proyecto.

---
+++ ActividadesDeCalificación/Problemasconarreglos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesDeCalificación
+{
+    class Problemasconarreglos
+    {
+        static void Main()
+        {
+            int mayor = 0;
+            string[] Nombres = new string[10];
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.WriteLine(" Escriba el nombre del estudiantes {0}: ",i);
+                Nombres[i] = Console.ReadLine();
+
+            }
+            int[] notas = new int[10];
+            for (int inc = 0; inc < notas.Length; inc++)
+            {
+                Console.WriteLine(" Escriba las notas del estudiante {0} ", inc);
+                notas[inc] = int.Parse(Console.ReadLine());
+                if (notas[inc] > mayor )
+                {
+                    mayor = notas[inc];
+                }
+            }
+
+            int suma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5] + notas[6] + notas[7] + notas[8] + notas[9];
+            double promedio = suma / 10;
+           for (int i= 0; i < Nombres.Length; i++)
+            {
+                Console.WriteLine("Estudiante {0}: {1} ", i, Nombres[i]);
+            }
+           for (int inc = 0; inc < notas.Length; inc++)
+            {
+                Console.WriteLine("Calificación estudiante {0}: {1} ", inc, notas[inc]);
+            }
+            Console.WriteLine(" El promedio de las de las notas de los estudiantes es: "+ promedio);
+            Console.WriteLine("La nota mayor es: " + mayor);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ActividadesDeCalificación/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesDeCalificación
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] notas = new int[10];
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("Escriba la nota {0}: ", i);
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("notas {0}: {1}", i, notas[i]);
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
