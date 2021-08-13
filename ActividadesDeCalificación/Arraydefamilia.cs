adc1f485762d4c7d13a9e619da50095078977d22
Saul Emmanuel Zuniga Villatoro <saulzuniga606@gmail.com>
2021-08-13 11:50:38 -0600

    ejercicio con arrays

---
+++ ActividadesDeCalificación/Arraydefamilia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesDeCalificación
+{
+    class Arraydefamilia
+    {
+        static void Main()
+        {
+            string[] miembros = new string[5];
+            for(int i=0; i<miembros.Length; i++)
+            {
+                Console.WriteLine("Ingrese el miembro {0}:", i);
+                miembros[i] = Console.ReadLine();
+            }
+            Array.Sort(miembros);
+            for (int i = 0;i<miembros.Length;i++)
+            {
+                Console.WriteLine(" Nombres de los integrantes {0}: {1} ", i, miembros[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
